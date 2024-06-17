7665abcfe0f68c2a51d0ad136ea0f89a571ec7dd
Leon Gebre <gelf.learing@gmail.com>
2024-06-17 22:17:28 +0300

    updating the redis client connection

--- utils/redis.js
+++ utils/redis.js
@@ -1,5 +1,27 @@
 import { createClient } from 'redis'
 
-const client = await createClient()
-  .on('error', err => console.log('Redis Client Error', err))
-  .connect();
+class RedisClient { 
+  constructor() {
+    const client = createClient()
+    .on('error', err => console.log('Redis Client Error', err))
+    .connect();
+  }
+
+  isalive() {
+    return this.client.isalive();
+  }
+
+  get (key) {
+    return this.client.get.key
+  }
+
+  set (key, value, time) {
+    this.client.set(key, value, 'EX', time)
+  }
+
+  del (key) {
+    this.client.del(key)
+  }
+}
+
+module.exports.RedisClient = redisClient;

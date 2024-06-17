dcbf9a707c1a689f73945354eb573d682e669226
Leon Gebre <gelf.learing@gmail.com>
2024-06-17 23:02:00 +0300

    this.clint wasn't defined correctly and um fixing it

--- utils/redis.js
+++ utils/redis.js
@@ -1,58 +1,58 @@
-import { createClient } from 'redis'
+import { createClient } from 'redis';
 
-class RedisClient { 
+class RedisClient {
   constructor() {
-    const client = createClient()
-    .on('error', err => console.log('Redis Client Error', err))
-    .connect();
+    this.client = createClient()
+      .on('error', err => console.log('Redis Client Error', err));
+    this.client.connect();
   }
 
   isalive() {
     return new Promise((resolve, reject) => {
-      this.client.ping((error, replay) => {
+      this.client.ping((error, reply) => {
         if (error) {
           reject(error);
         } else {
-          resolve(replay === 'PONG');
-        }
-      });
-    })
-  }
-
-  async get (key) {
-    return new Promise((resolve, reject) => {
-      this.client.get(key, (error, replay) => {
-        if (error){
-          reject(error);
-        } else {
-          resolve(replay);
+          resolve(reply === 'PONG');
         }
       });
     });
   }
 
-  async set (key, value, time) {
+  async get(key) {
     return new Promise((resolve, reject) => {
-      this.client.set(key, value, 'EX', time, (error, replay) => {
+      this.client.get(key, (error, reply) => {
         if (error) {
           reject(error);
         } else {
-          resolve(replay);
+          resolve(reply);
         }
-      })
+      });
     });
   }
 
-  del (key) {
-    return new Promise((resolve, reject) => { 
-      this.client.del(key, (replay, error) => {
-        if (error){
+  async set(key, value, time) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', time, (error, reply) => {
+        if (error) {
           reject(error);
         } else {
-          resolve(replay);
+          resolve(reply);
         }
       });
-     });
+    });
+  }
+
+  del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (error, reply) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
   }
 }
 

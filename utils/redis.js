6905ac3eeb2587ccee8fd510c9eda250081ac855
Leon Gebre <gelf.learing@gmail.com>
2024-06-17 22:58:59 +0300

    making all methods async fucntin

--- utils/redis.js
+++ utils/redis.js
@@ -8,19 +8,51 @@ class RedisClient {
   }
 
   isalive() {
-    return this.client.isalive();
+    return new Promise((resolve, reject) => {
+      this.client.ping((error, replay) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(replay === 'PONG');
+        }
+      });
+    })
   }
 
-  get (key) {
-    return this.client.get.key
+  async get (key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (error, replay) => {
+        if (error){
+          reject(error);
+        } else {
+          resolve(replay);
+        }
+      });
+    });
   }
 
-  set (key, value, time) {
-    this.client.set(key, value, 'EX', time)
+  async set (key, value, time) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', time, (error, replay) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(replay);
+        }
+      })
+    });
   }
 
   del (key) {
-    this.client.del(key)
+    return new Promise((resolve, reject) => { 
+      this.client.del(key, (replay, error) => {
+        if (error){
+          reject(error);
+        } else {
+          resolve(replay);
+        }
+      });
+     });
   }
 }
 

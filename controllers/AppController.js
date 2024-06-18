f5903d79d5aa4999e39219623d92bdc02451588f
Leon Gebre <gelf.learing@gmail.com>
2024-06-18 21:56:09 +0300

    fxing the relation between the rout and papcontroller

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,11 +1,12 @@
-import {express} from 'express';
 import { dbClient } from '../utils/db';
 import { redisClient } from '../utils/redis';
 
-async function getStatus (req, res) {
+export async function getStatus(req, res) {
   res.status(200).json({ "redis": redisClient.isAlive(), "db": dbClient.isAlive() });
 }
 
-async function getStats (req, res) {
-  res.status(200).json({ 'users' : dbClient.nbUsers(), 'files' : dbClient.nbFiles() });
+export async function getStats(req, res) {
+  const usersCount = await dbClient.nbUsers();
+  const filesCount = await dbClient.nbFiles();
+  res.status(200).json({ 'users': usersCount, 'files': filesCount });
 }

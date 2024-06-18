8b090b8c337cfe2a1f9ec6a271d0cba03c1b28ab
Leon Gebre <gelf.learing@gmail.com>
2024-06-18 20:26:34 +0300

    updating the syntax errors and also the way i use process

--- utils/db.js
+++ utils/db.js
@@ -1,14 +1,24 @@
-import MongoClient from "mongodb";
+import MongoClient from 'mongodb';
 
-const DB_HOST = process.env('DB_HOST') || 'localhost';
-const DB_PORT = process.env('DB_PORT') || 27017;
-const DB_DATABASE = process.env('DB_DATABASE') || 'files_manager';
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
 class DBClient {
   constructor() {
     this.url = `mongodb://${DB_HOST}:${DB_PORT}`;
-    this.client = MongoClient(this.url);
-    this.db = this.client.db(DB_DATABASE);
+    this.connect(); 
   }
+
+  async connect() {
+    try {
+      this.client = await MongoClient.connect(this.url, { useNewUrlParser: true, useUnifiedTopology: true });
+      this.db = this.client.db(DB_DATABASE);
+      console.log('Successfully connected to MongoDB');
+    } catch (error) {
+      console.error('Connection to MongoDB failed', error);
+    }
+  }
+}
 
   isAlive() {
     if (this.db) return true;
@@ -24,5 +34,5 @@ class DBClient {
   }
 }
 
-const dbClient = DBClient();
+const dbClient = new DBClient();
 export default dbClient;

25f6b0f912a69731cf1c2a58be004364986091f0
Leon Gebre <gelf.learing@gmail.com>
2024-06-18 22:22:54 +0300

    making app controller a class inseted of normal function

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,12 +1,22 @@
-import { dbClient } from '../utils/db';
-import { redisClient } from '../utils/redis';
+import RedisClient from '../utils/redis';
+import DBClient from '../utils/db';
 
-export async function getStatus(req, res) {
-  res.status(200).json({ "redis": redisClient.isAlive(), "db": dbClient.isAlive() });
+class AppController {
+  static getStatus(request, response) {
+    const status = {
+      redis: RedisClient.isAlive(),
+      db: DBClient.isAlive(),
+    };
+    return response.status(200).send(status);
+  }
+
+  static async getStats(request, response) {
+    const stats = {
+      users: await DBClient.nbUsers(),
+      files: await DBClient.nbFiles(),
+    };
+    return response.status(200).send(stats);
+  }
 }
 
-export async function getStats(req, res) {
-  const usersCount = await dbClient.nbUsers();
-  const filesCount = await dbClient.nbFiles();
-  res.status(200).json({ 'users': usersCount, 'files': filesCount });
-}
+module.exports = AppController;

05948f65edc7377a90360cbefb866949ad195898
Leon Gebre <gelf.learing@gmail.com>
2024-06-18 16:48:31 +0300

    connecting to my local mongodb using mondbclient

---
+++ utils/db.js
@@ -0,0 +1,27 @@
+import MongoClient from "mongodb";
+
+const DB_HOST = process.env('DB_HOST') || 'localhost';
+const DB_PORT = process.env('DB_PORT') || 27017;
+const DB_DATABASE = process.env('DB_DATABASE') || 'files_manager';
+class DBClient {
+  constructor() {
+    this.url = `mongodb://${DB_HOST}:${DB_PORT}`;
+    this.client = MongoClient(this.url);
+    this.db = this.client.db(DB_DATABASE);
+  }
+
+  isAlive() {
+    if (this.db) return true;
+    return false;
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+}

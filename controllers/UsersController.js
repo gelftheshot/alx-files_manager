0a62f4ebe81b66a429ee5ea204022bbc97fcdae8
Leon Gebre <gelf.learing@gmail.com>
2024-06-19 21:08:51 +0300

    flolowing some common rulers of writign varibale in javascript

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,24 +3,19 @@ import dbClient from '../utils/db';
 
 class UsersController {
   static async postNew(request, response) {
-    const useremail = request.body.email;
-    const userpassword = request.body.password;
+    const userEmail = request.body.email;
+    if (!userEmail) return response.status(400).send({ error: 'Missing email' });
 
-    if (!useremail) {
-      return response.status(400).send({ error: 'Missing email' });
-    }
-    if (!userpassword) {
-      return response.status(400).send({ error: 'Missing password' });
-    }
+    const userPassword = request.body.password;
+    if (!userPassword) return response.status(400).send({ error: 'Missing password' });
 
-    if (await dbClient.db.collection('user').findOne({ email: useremail })) {
-      response.status(400).send({ error: 'Already exist' });
-    }
+    const oldUserEmail = await dbClient.db.collection('users').findOne({ email: userEmail });
+    if (oldUserEmail) return response.status(400).send({ error: 'Already exist' });
 
-    const hpwd = sha1(userpassword);
-    const newuser = await dbClient.db.collection('user').insertOne({ email: useremail, password: hpwd });
+    const shaUserPassword = sha1(userPassword);
+    const result = await dbClient.db.collection('users').insertOne({ email: userEmail, password: shaUserPassword });
 
-    return response.status(201).send({ id: newuser.insertedId, email: useremail });
+    return response.status(201).send({ id: result.insertedId, email: userEmail });
   }
 }
 
